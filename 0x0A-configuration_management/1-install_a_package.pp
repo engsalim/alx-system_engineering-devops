0ef38e6cb3ddeb5c9afe089271e299214bf9de39
engsalim <39193592+engsalim@users.noreply.github.com>
2022-10-14 23:15:34 +0300

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,5 @@
-# install puppet-lint ver 2.5.0
-
-package { 'puppet-lint':
-  ensure   => '2.5.0',
-  provider => 'gem',
-  source   => 'https://rubygems.org/gems/puppet-lint/versions/2.5.0',
+# install flask from pip3.
+package { 'flask':
+    ensure   => '2.1.0',
+    provider => 'pip3',
 }
